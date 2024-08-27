fa5a00f68350a4f5bf0c8333e0a7c30b01b87b00
sviatoslav sukharev <sviatoslav.sukharev@yandex.ru>
2024-08-27 22:41:23 +0300

    feat(welcome): add query param for welcome page

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,6 +1,15 @@
+import { useSearchParams } from "react-router-dom";
+
 export const Welcome = () => {
+    const [searchParams] = useSearchParams()
+
+    const id = searchParams.get('id')
+
     return (
         <div>
+            <h1>
+                id = { id }
+            </h1>
             <h1>
                 Welcome PAGE
             </h1>
